1806039da436ab687f6254dea330814a8381e735
Mena A <168314199+Mena22222@users.noreply.github.com>
2024-08-17 08:01:16 -0300

    solucionado

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-const midiv = document.getElementById("midiv");
+const midiv = document.querySelector("div");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", event => {
+    event.stopPropagation();
+    alert ("Hola!");
+})
 
 midiv.addEventListener("click", function (){
   alert( 'Hola, soy el div!' );
